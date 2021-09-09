7f7445b4cb52dcbd9f57172e483a723d28673f42
geraldc118 <geraldcameron118@gmail.com>
2021-09-09 01:13:03 +0100

    Components Error-replaced all components back to pre build without css intialised

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import './css/App.css';
 import { BrowserRouter } from "react-router-dom"
-import Home from "./Components/Home.js";
+import Header from "./Components/Header.js";
 import Navbar from "./Components/Navbar.js"
 import About from "./Components/About.js";
 import Projects from "./Components/Projects.js";
 import Footer from "./Components/Footer.js";
-import Contact from "./Components/Contact";
+// import Contact from "./Components/Contact";
 
 
 
 function App () {
   return (<BrowserRouter>
    <Navbar />
-   <Home />
+   <Header />
    <About />
    <Projects />
    {/* <Contact /> */}
