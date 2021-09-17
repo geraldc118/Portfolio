611f95183ba2c969108452af46a9154bcc1fd687
geraldc118 <geraldcameron118@gmail.com>
2021-09-17 23:07:52 +0100

    Completed contact page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from "./Components/Navbar.js"
 import About from "./Components/About.js";
 import Projects from "./Components/Projects.js";
 import Footer from "./Components/Footer.js";
-// import Contact from "./Components/Contact";
+import Contact from "./Components/Contact";
 
 
 
@@ -16,7 +16,7 @@ function App () {
    <Header />
    <About />
    <Projects />
-   {/* <Contact /> */}
+   <Contact />
    <Footer />
   </BrowserRouter>
   );
